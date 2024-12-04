b994bb41c23bf5ee81f82f864dfbea47c192481f
Max <119303798+MissingPaw@users.noreply.github.com>
2024-12-04 20:56:49 +0800

    Delete Perturbation directory

--- Perturbation/files/Get_para.m
+++
@@ -1,9 +0,0 @@
-function parameters = Get_para(sympara)
-    parameters = struct();
-    for i = 1: length(sympara)
-        paraname = char(sympara(i));
-        paravalue = evalin('base',paraname);
-        parameters.(paraname) = paravalue;
-    end
-end
-
